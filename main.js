5050c8725cf8909e2956acd819c07f814897a175
cutecactusbunny <105020497+cutecactusbunny@users.noreply.github.com>
2022-09-17 16:59:05 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+
+function new_image()
+{
+	fabric.Image.fromURL('BirthdayImage.Jpg', function(Img) {
+    block_image_object = Img;    
+	
+    block_image_object.scaleToWidth(700);
+    block_image_object.scaleToHeight(510);
+    block_image_object.set({
+    top:0,
+    left:0
+    });
+    Canvas.add(block_image_object);
+});
+
+}
+
+function playSound(){
+	X.play();
+}
